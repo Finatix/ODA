4fef02641ccd961ab3cea4e460687051098ff256
a.schmidt <a.schmidt@finatix.de>
2024-05-30 16:02:42 +0200

    Adds conversation management with local storage.
    Stores meta information about conversations and chats in local storage.
    Reads from storage to manipulate and render the respective
    conversations.
    
    Also alters some colors and introduces hover effects on interactive
    elements.

--- backend/src/main/resources/static/scripts.js
+++ backend/src/main/resources/static/scripts.js
@@ -5,8 +5,6 @@ async function fetchDataFromAPI(url, options) {
         const json = await response.json();
         decreaseLoadingCounter();
         return json;
-        //console.log(result.conversationToken);
-        //console.log(response.json());
     } catch (error) {
         console.error(error);
         decreaseLoadingCounter();
@@ -15,106 +13,68 @@ async function fetchDataFromAPI(url, options) {
 let baseUrl = window.origin
 let postConversationsURL = `${baseUrl}/api/conversations`;
 let postConversationsOptions = {
-    method: 'POST',
+    method: "POST",
     headers: {
-        'Access-Allow-Origin': "*"
+        "Access-Allow-Origin": "*"
     }
 }
 
-/* get whole messages list to append it to the chat body */
-async function getMessages (conversationToken) {
-    let getMessagesURL = `https://raw.githubusercontent.com/api/conversations/${conversationToken}/messages`
-    getMessagesOptions = {
-        method: 'GET',
-        headers: {
-            'conversationToken': conversationToken
-        }
-    }
-    result = fetchDataFromAPI(getMessagesURL, getMessagesOptions);
-    return result;
-}
-let conversationToken
-function establishConversation() {
-    // if we don't have conversationToken, make a post and retrieve it. Also when we do have it already, load the list of messages.
-    conversationToken = localStorage.getItem('conversationToken');
-    if (true || !conversationToken)
-    {
+async function postMessage (message) {
+    let conversationList = JSON.parse(localStorage.getItem("conversations"));
+    let activeConversationIndex = conversationList.findIndex((el) => el.active);
+    let conversationToken = conversationList[activeConversationIndex].conversationToken;
 
-        fetchDataFromAPI(postConversationsURL, postConversationsOptions).then((result) => {
-            conversationToken = result.conversationToken
-            localStorage.setItem('conversationToken', conversationToken)
-        })
-    } else {
-        //messages = getMessages(conversationToken);
-        // Function to display messages in the chat interface
-
-        /* for testing purposes only */
-        const messages = [{
-            id: "1",
-            sender: "user",
-            timestamp: "2024-04-14T10:00:00",
-            conversation: "123456",
-            text: "Hello, how can I help you?"
-        },
-            {
-                id: "2",
-                sender: "AI",
-                timestamp: "2024-04-14T10:01:00",
-                conversation: "123456",
-                text: "Hello! I'm here to assist you."
-            },
-            // Add more messages here...
-        ];
-
-        let chatMessages = document.getElementById('chat-messages');
-        messages.forEach(message => {
-            const messageElement = document.createElement('div');
-            messageElement.textContent = `${message.sender}: ${message.text}`;
-            // Add CSS class based on the sender
-            if (message.sender === 'user') {
-                messageElement.classList.add('message', 'user-message');
-            } else if (message.sender === 'AI') {
-                messageElement.classList.add('message', 'ai-message');
-            }
-            chatMessages.appendChild(messageElement);
-        });
-    }
-}
-
-
-async function postMessage (conversationToken,message) {
-    let postConversationsMessagesURL = `${baseUrl}/api/conversations/${conversationToken}/messages`
+    let postConversationsMessagesURL = `${baseUrl}/api/conversations/${conversationToken}/messages`;
     let postConversationsMessagesOptions = {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'content-type': 'application/json',
-            'conversationToken': conversationToken,
+            "content-type": "application/json",
+            "conversationToken": conversationToken,
         },
         body: JSON.stringify({
-            'message': message
-        })
-    }
-    let result
+            "message": message})
+    };
+
+    let sendTime = new Date().toLocaleTimeString();
+    conversationList[activeConversationIndex].messages.push({
+        text: message,
+        sender: "user",
+        time: sendTime,
+    });
+    conversationList.lastMessageTime = sendTime;
+    localStorage.setItem("conversations", JSON.stringify(conversationList));
+    renderConversationList();
+    addMessageToChat(message, "user", sendTime);
+
     fetchDataFromAPI(postConversationsMessagesURL, postConversationsMessagesOptions).then((response) => {
         let answer = response.response
-        addMessageToChat(answer, "ai");
+        let receiveTime = new Date().toLocaleTimeString();
+
+        addMessageToChat(answer, "ai", receiveTime);
+        conversationList[activeConversationIndex].messages.push({
+            text: answer,
+            sender: "ai",
+            time: receiveTime,
+        })
+        conversationList.lastMessageTime = receiveTime;
+        localStorage.setItem("conversations", JSON.stringify(conversationList));
+        renderConversationList();
     });
-    return result
 }
 
 // Function to send message
 function sendMessage(event) {
-    let messageInput = document.querySelector('#message-input');
+    let messageInput = document.querySelector("#message-input");
     let message = messageInput.value;
-    addMessageToChat(message, "user");
-    messageInput.value = ''; // Clear input field after sending message
-    postMessage(conversationToken, message);
+    messageInput.value = "";
+    postMessage(message);
 }
 
-function addMessageToChat(message, author) {
-    let chatMessages = document.getElementById('chat-messages');
-    let messageContainer = document.createElement('div');
-    messageContainer.className = 'message-container ' + author + '-message';
+function addMessageToChat(message, author, time) {
+    let chatMessages = document.getElementById("chat-messages");
+    let messageContainer = document.createElement("div");
+    messageContainer.classList.add("message-container");
+    messageContainer.classList.add(author + "-message");
     let authorImage = (() => {
         if (author === "user") {
             let userImage = document.querySelector("#user-blueprint");
@@ -125,17 +85,17 @@ function addMessageToChat(message, author) {
         }
     })();
     authorImage.id = "";
-    authorImage.className = 'avatar';
+    authorImage.className = "avatar";
     authorImage.style.display = "";
-    let messageContent = document.createElement('div');
-    messageContent.className = 'message-content';
-    let senderHeadline = document.createElement('span');
-    senderHeadline.className = 'message-sender';
-    senderHeadline.innerText = author === 'user' ? 'Du' : 'ODA';
-    let timeHeadline = document.createElement('span');
-    timeHeadline.className = 'message-time';
-    timeHeadline.innerText = new Date().toLocaleTimeString();
-    let messageNode = document.createElement('p');
+    let messageContent = document.createElement("div");
+    messageContent.className = "message-content";
+    let senderHeadline = document.createElement("span");
+    senderHeadline.className = "message-sender";
+    senderHeadline.innerText = author === "user" ? "Du" : "ODA";
+    let timeHeadline = document.createElement("span");
+    timeHeadline.className = "message-time";
+    timeHeadline.innerText = !time ? new Date().toLocaleTimeString() : time;
+    let messageNode = document.createElement("p");
     messageNode.innerText = message;
     messageContent.appendChild(senderHeadline);
     messageContent.appendChild(timeHeadline);
@@ -144,32 +104,6 @@ function addMessageToChat(message, author) {
     messageContainer.appendChild(messageContent);
     if(author === "user") messageContainer.appendChild(authorImage);
     chatMessages.appendChild(messageContainer);
-}
-
-function addToConversation(answer) {
-    // Simulate AI response (replace with actual AI response)
-    let chatMessages = document.getElementById('chat-messages');
-    let aiResponseDiv = document.createElement('div');
-    aiResponseDiv.className = 'message ai-message'; // AI message
-    console.log(conversationToken);
-
-    aiResponseDiv.textContent = answer;
-    chatMessages.appendChild(aiResponseDiv);
-}
-
-waitForElements()
-function waitForElements() {
-    let chatboxButton = document.querySelector('#chat-box button');
-    if(!chatboxButton) {
-        setTimeout(waitForElements, 200);
-    } else {
-        initializeApp();
-    }
-}
-
-function initializeApp() {
-    establishConversation();
-    addEventListeners();
 }
 
 let _loadCounter = 0;
@@ -184,32 +118,166 @@ function decreaseLoadingCounter() {
 }
 
 function checkLoadingSpinner() {
-    const spinner = document.querySelector('.loading-spinner');
+    const spinner = document.querySelector(".loading-spinner");
     if (!spinner) { return; }
 
-    spinner.setAttribute('data-counter', `${_loadCounter}`);
+    spinner.setAttribute("data-counter", `${_loadCounter}`);
     if (_loadCounter > 0) {
-        spinner.classList.add('active');
+        spinner.classList.add("active");
     } else {
-        spinner.classList.remove('active');
+        spinner.classList.remove("active");
     }
 }
 
 function addEventListeners() {
     // Event listener for mobile aside toggle
-    document.querySelector('.mobile-toggle').addEventListener('click', () => {
-        document.querySelector('.chat-aside').classList.toggle('visible');
+    document.querySelector(".mobile-toggle").addEventListener("click", () => {
+        document.querySelector(".chat-aside").classList.toggle("visible");
     });
 
     // Event listener for send button click
-    document.querySelector('#send-message-button').addEventListener('click', () => {
+    document.querySelector("#send-message-button").addEventListener("click", () => {
         sendMessage(conversationToken);
     });
 
     // Event listener for Enter key press
-    document.querySelector('#message-input').addEventListener('keypress', e => {
-        if (e.key === 'Enter') {
+    document.querySelector("#message-input").addEventListener("keypress", e => {
+        if (e.key === "Enter") {
             sendMessage();
         }
     });
+
+    // Event listener for add conversation button
+    document.querySelector(".add-conversation").addEventListener("click", () => {
+        addConversation();
+    });
+}
+
+function addConversation(isActive) {
+    let conversations = JSON.parse(localStorage.getItem("conversations"));
+    if(!conversations) conversations = [];
+    if(MAX_CONVERSATIONS <= conversations.length) {
+        console.error("You have reached the limit of conversations. Please empty your cache if would like to create a new one.")
+        return 0;
+    }
+    fetchDataFromAPI(postConversationsURL, postConversationsOptions).then((result) => {
+        conversationToken = result.conversationToken;
+        let conversationId = idCounter.getNextId();
+        let creationTime = new Date().toLocaleTimeString();
+        let newConversation = {
+            title: "Conversation-" + (conversations.length+1),
+            id: conversationId,
+            nodeId: "conv-id-" + conversationId,
+            conversationToken: conversationToken,
+            active: Boolean(isActive),
+            lastMessageTime: creationTime,
+            messages: [
+                {
+                    time: creationTime,
+                    sender: "ai",
+                    text: ODA_GREETING_TEXT,
+                },
+            ],
+        };
+        conversations.push(newConversation);
+        localStorage.setItem("conversations", JSON.stringify(conversations));
+        renderConversationList();
+        if(newConversation.active) {
+            changeConversation(null, newConversation);
+        }
+    });
+}
+
+function changeConversation(event, conversationItem) {
+    let conversationList = JSON.parse(localStorage.getItem("conversations"));
+    conversationList = conversationList.map((el) => {
+        el.active = el.id === conversationItem.id;
+        return el;
+    });
+    localStorage.setItem("conversations", JSON.stringify(conversationList));
+    renderConversationList();
+    document.querySelector("#active-chat-title").innerText = conversationItem.title;
+    renderMessages(conversationItem.messages);
+}
+
+function renderConversationList() {
+    let conversationList = document.querySelector(".conversation-list");
+    Array.from(conversationList.children).forEach(el => conversationList.removeChild(el))
+    let conversations = JSON.parse(localStorage.getItem("conversations"));
+    conversations = conversations.sort((a, b) => a.lastMessageTime < b.lastMessageTime);
+    conversations.forEach((conversation) => {
+        addConversationNode(conversation);
+    });
+}
+
+function addConversationNode(conversationItem) {
+    let conversationList = document.querySelector(".conversation-list");
+    let articleNode = document.createElement("article");
+    articleNode.id = conversationItem.nodeId;
+    articleNode.classList.add("conversation-item");
+    if(conversationItem.active) {
+        articleNode.classList.add("active");
+    }
+    articleNode.addEventListener("click", (event) => { changeConversation(event, conversationItem); });
+    let convHeading = document.createElement("p");
+    convHeading.className = "conversation-item-name font-weight-bold";
+    convHeading.innerText = conversationItem.title;
+    let convPreview = document.createElement("p");
+    convPreview.className = "conversation-preview";
+    convPreview.innerText = conversationItem.messages[conversationItem.messages.length-1].text.slice(0,30) + "...";
+    articleNode.appendChild(convHeading);
+    articleNode.appendChild(convPreview);
+    conversationList.appendChild(articleNode);
+}
+
+function renderMessages(messages) {
+    let chatBox = document.querySelector("#chat-messages");
+    Array.from(chatBox.children).forEach(el => chatBox.removeChild(el));
+    messages.forEach(msg => addMessageToChat(msg.text, msg.sender, msg.time));
+}
+
+const MAX_CONVERSATIONS = 10;
+const ODA_GREETING_TEXT = "Hallo! Ich bin ODA, die Open Data Assistant der Stadt Leipzig. Ich helfe Dir gerne beim Durchsuchen der Open Data Kataloge der Partnerstädte Leipzig, Hamburg, Berlin und München. Wähle dazu oben rechts deine gewünschte Stadt aus.";
+
+waitForElements()
+function waitForElements() {
+    let chatboxButton = document.querySelector("#chat-box button");
+    if(!chatboxButton) {
+        setTimeout(waitForElements, 200);
+    } else {
+        initializeApp();
+    }
+}
+
+let idCounter;
+function initializeApp() {
+    let baseConvId = 0;
+    let conversationList = JSON.parse(localStorage.getItem("conversations"));
+    if(conversationList) {
+        baseConvId = conversationList.sort((a, b) => a.id < b.id)[0] + 1;
+    }
+    idCounter = (function incrementId() {
+        let startId = baseConvId;
+        return {
+            getNextId() {
+                return startId++;
+            }
+        }
+    })();
+
+    addEventListeners();
+
+    if(!conversationList) {
+        addConversation(true);
+    } else {
+        renderConversationList();
+        let activeConversations = conversationList.filter(el => el.active > 0);
+        if(0 < activeConversations.length) {
+            changeConversation(null, activeConversations[0]);
+        }
+    }
+}
+
+function logConversations() {
+    return JSON.parse(localStorage.getItem("conversations"));
 }
