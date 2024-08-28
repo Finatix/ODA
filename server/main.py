8c64336324ad6bd5ea95ee7a4f9a57beef0ead30
Kai Börner <k.boerner@finatix.de>
2024-08-28 17:31:49 +0200

    simplify static content

--- server/main.py
+++ server/main.py
@@ -1,35 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import FileResponse,  HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-from typing import Annotated
+
+# settings
+
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "resources" / "static"
+HTML_DIR = STATIC_DIR / "html"
+
+# webserver
 
 app = FastAPI()
 
-# webserver api
+## static files
+app.mount("/static", StaticFiles(directory=STATIC_DIR))
 
-@app.get("/", response_class=HTMLResponse)
+## frontend
+@app.head("/")
+@app.get("/")
 async def index():
-    return read_html_file("index.html")
-
-@app.get("/static/{resource:path}", response_class=FileResponse)
-async def resource(resource: Annotated[str, 'resource path e.g. "styles.css"']):
-    file = get_resource_path() / resource
-
-    if not file.is_file():
-        raise HTTPException(status_code=404, detail=f"File not found: {resource}")
-
-    return file
-
-
-# helpers
-
-def get_html_dir() -> Path:
-    return get_resource_path() / "html"
-
-def get_resource_path() -> Path:
-    return Path(__file__).resolve().parent / "resources" / "static"
-
-def read_html_file(file: str) -> str:
-    html_file = get_html_dir() / file
-
-    return html_file.read_text()
+    return FileResponse(HTML_DIR / "index.html")
