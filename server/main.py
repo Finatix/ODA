38c04092fc23f3efff67504f97e381059eb6faed
Kai Börner <k.boerner@finatix.de>
2024-08-28 14:50:09 +0200

    main.py

--- server/main.py
+++ server/main.py
@@ -1,14 +1,15 @@
 #! /bin/env python3
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 from pathlib import Path
 
 app = FastAPI()
 
 # webserver api
 
-@app.get("/")
-def index():
+@app.get("/", response_class=HTMLResponse)
+async def index():
     return read_html_file("index.html")
 
 # helpers
@@ -16,7 +17,7 @@ def index():
 def get_html_dir() -> Path:
     return Path(__file__).parent / "resources" / "static" / "html"
 
-def read_html_file(file: str):
+def read_html_file(file: str) -> str:
     html_file = get_html_dir() / file
 
-    return html_file.read_bytes()
+    return html_file.read_text()
