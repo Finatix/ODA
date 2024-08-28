de715e6cfd837c5037018a6e1d5f92c1f42c9bdd
Kai Börner <k.boerner@finatix.de>
2024-08-28 15:55:19 +0200

    static resources

--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 #! /bin/env python3
 
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse,  HTMLResponse
 from pathlib import Path
+from typing import Annotated
 
 app = FastAPI()
 
@@ -12,10 +13,23 @@ app = FastAPI()
 async def index():
     return read_html_file("index.html")
 
+@app.get("/static/{resource:path}", response_class=FileResponse)
+async def resource(resource: Annotated[str, 'resource path e.g. "styles.css"']):
+    file = get_resource_path() / resource
+
+    if not file.is_file():
+        raise HTTPException(status_code=404, detail=f"File not found: {resource}")
+
+    return file
+
+
 # helpers
 
 def get_html_dir() -> Path:
-    return Path(__file__).parent / "resources" / "static" / "html"
+    return get_resource_path() / "html"
+
+def get_resource_path() -> Path:
+    return Path(__file__).resolve().parent / "resources" / "static"
 
 def read_html_file(file: str) -> str:
     html_file = get_html_dir() / file
