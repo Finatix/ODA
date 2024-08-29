f6e337c5f3a3914e225b04e6e617e148ebf424aa
Kai Börner <k.boerner@finatix.de>
2024-08-29 15:22:56 +0200

    components.api __all__

--- server/components/api/__init__.py
+++ server/components/api/__init__.py
@@ -3,3 +3,11 @@ from .ConversationResponse import ConversationResponse
 from .Message import Message
 from .PastConversation import PastConversation
 from .UserMessage import UserMessage
+
+__all__ = [
+    "Conversation",
+    "ConversationResponse",
+    "Message",
+    "PastConversation",
+    "UserMessage"
+]
