4d3542bb59f3af090bfef3348d28a37aee97529d
Kai Börner <k.boerner@finatix.de>
2024-08-30 17:14:37 +0200

    session_verifier

--- server/components/session/SessionVerifier.py
+++ server/components/session/SessionVerifier.py
@@ -1,38 +1,36 @@
 from .SessionData import SessionData
+from fastapi_sessions.backends import SessionBackend
 from fastapi_sessions.backends.implementations import InMemoryBackend
 from fastapi_sessions.session_verifier import SessionVerifier as __SessionVerifier
 from fastapi import HTTPException
+from typing import override
 from uuid import UUID
 
 class SessionVerifier(__SessionVerifier[UUID, SessionData]):
-    def __init__(
-        self,
-        *,
-        identifier: str,
-        auto_error: bool,
-        backend: InMemoryBackend[UUID, SessionData],
-        auth_http_exception: HTTPException,
-    ):
-        self._identifier = identifier
-        self._auto_error = auto_error
-        self._backend = backend
-        self._auth_http_exception = auth_http_exception
+    def __init__(self):
+        self.__backend: SessionBackend[UUID, SessionData] = None
 
+    @override
     @property
-    def identifier(self):
-        return self._identifier
+    def identifier(self) -> str:
+        return "session_verifier"
 
+    @override
     @property
-    def backend(self):
-        return self._backend
+    def backend(self) -> SessionBackend[UUID, SessionData]:
+        if self.__backend is None:
+            self.__backend = InMemoryBackend[UUID, SessionData]()
+        return self.__backend
 
+    @override
     @property
-    def auto_error(self):
-        return self._auto_error
+    def auto_error(self) -> bool:
+        return True
 
+    @override
     @property
-    def auth_http_exception(self):
-        return self._auth_http_exception
+    def auth_http_exception(self) -> HTTPException:
+        return HTTPException(status_code=403, detail="invalid session")
 
     def verify_session(self, model: SessionData) -> bool:
         """If the session exists, it is valid"""
