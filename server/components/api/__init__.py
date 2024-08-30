75eac51c0756d43bf4b2f2af4588cb90963c59a8
Kai Börner <k.boerner@finatix.de>
2024-08-30 14:58:57 +0200

    session data

--- server/components/api/__init__.py
+++ server/components/api/__init__.py
@@ -6,3 +6,4 @@ from .ConversationResponse import ConversationResponse
 from .Message import Message
 from .PastConversation import PastConversation
 from .UserMessage import UserMessage
+from .SessionData import SessionData
