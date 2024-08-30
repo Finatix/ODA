f9123c1564ebf5e444abfac0019b87c5d09afbb4
Kai Börner <k.boerner@finatix.de>
2024-08-30 14:12:31 +0200

    configure the generator service

--- server/services.py
+++ server/services.py
@@ -1,14 +1,25 @@
 """service definitions for FastAPI dependeny injection
 """
 
+from .components.ai import Generator, MockGenerator
 from .components.conversation import ConversationManager, MockConversationManager
 
 def conversation_manager() -> ConversationManager:
     """creates the ConversationManager service
 
-    For starter. it's just the MockConversationManager
+    For starters it's just the MockConversationManager
 
     Returns:
         ConversationManager: the service to be injected
     """
     return MockConversationManager()
+
+def generator() -> Generator:
+    """creates the Generator service
+
+    For starters its just the MockGenerator
+
+    Returns:
+        Generator: the service to be injected
+    """
+    return MockGenerator()
