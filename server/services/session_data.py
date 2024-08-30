70b9b774d7ac3d3ebcb28c8497d8c67d092a8e5b
Kai Börner <k.boerner@finatix.de>
2024-08-30 17:20:53 +0200

    session_data

--- server/services/session_data.py
+++ server/services/session_data.py
@@ -2,6 +2,9 @@
 """
 
 from ..components.session import SessionData
+from .session_verifier import session_verifier
+from fastapi import Depends
+from typing import Annotated
 
-def session_data() -> SessionData:
-    return SessionData()
+def session_data(session_data: Annotated[SessionData, Depends(session_verifier)]) -> SessionData:
+    return session_data
