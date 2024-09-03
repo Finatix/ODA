0fbc459d216d8b848ac4cd6d69648d236127676a
Kai Börner <k.boerner@finatix.de>
2024-09-03 17:24:48 +0200

    changes for usability in the WeaviateConversationmanager

--- server/components/api/Conversation.py
+++ server/components/api/Conversation.py
@@ -1,5 +1,6 @@
-from .Message import Message
+from .Message import Message, timestamp
 from .PastConversation import PastConversation
+from datetime import datetime
 from pydantic import BaseModel, Field
 from typing import Annotated
 from uuid import UUID, uuid4
@@ -8,10 +9,12 @@ class Conversation(BaseModel):
     token: Annotated[UUID, "the token of the conversation", Field(default_factory=uuid4)]
     title: Annotated[str, "the optional title of the conversation", Field(default="")]
     messages: Annotated[list[Message], "the message list", Field(default=[])]
+    timestamp: Annotated[datetime, "the timestamp", Field(default_factory=timestamp)]
 
     def add_message(self, message: str, sender: str) -> None:
         message = Message(text=message, sender=sender, conversation=self.token)
         self.messages.append(message)
+        self.timestamp = message.timestamp
 
     def to_past_conversation(self) -> PastConversation:
         return PastConversation(title=self.title, id=self.token)
--- server/components/api/Message.py
+++ server/components/api/Message.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+from datetime import datetime, UTC
 from pydantic import BaseModel, Field
 from typing import Annotated
 from uuid import UUID, uuid4
 
-def get_current_timestamp() -> str:
-    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+def timestamp() -> datetime:
+    return datetime.now(UTC)
 
 class Message(BaseModel):
-    id: Annotated[UUID, "the UUID of the message", Field(default_factory=uuid4)]
+    id: Annotated[UUID, "the UUID of the message", Field(default_factory=uuid4, alias="message_id")]
     text: Annotated[str, "the message text"]
     sender: Annotated[str, "the message sender"]
-    timestamp: Annotated[str, "the timestamp", Field(default_factory=get_current_timestamp)]
+    timestamp: Annotated[datetime, "the timestamp", Field(default_factory=timestamp)]
     conversation: Annotated[UUID, "the token of the conversation"]
