9fa223c0be24861b06b4a13fb82aa2598c4fba18
Kai Börner <k.boerner@finatix.de>
2024-09-04 10:38:55 +0200

    secure parameter

--- server/services/weaviate_settings.py
+++ server/services/weaviate_settings.py
@@ -8,9 +8,9 @@ def weaviate_settings() -> WeaviateSettings:
     return WeaviateSettings(
         http_host=getenv("WEAVIATE_HTTP_HOST", "0.0.0.0"),
         http_port=getenv("WEAVIATE_HTTP_PORT", 9090),
-        http_secure=False,
+        http_secure=getenv("WEAVIATE_HTTP_SECURE", False),
         grpc_host=getenv("WEAVIATE_GRPC_HOST", getenv("WEAVIATE_HTTP_HOST", "0.0.0.0")),
         grpc_port=getenv("WEAVIATE_GRPC_PORT", 50051),
-        grpc_secure=False,
+        grpc_secure=getenv("WEAVIATE_GRPC_SECURE", False),
         skip_init_checks=True
     )
