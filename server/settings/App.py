5104bceea3ad60f8bb6c6c04f0a27c27cee7a3e2
Kai Börner <k.boerner@finatix.de>
2024-09-04 15:43:11 +0200

    remove app settings

--- server/settings/App.py
+++
@@ -1,8 +0,0 @@
-from .OpenAi import OpenAi
-from .Weaviate import Weaviate
-from pydantic_settings import BaseSettings
-from typing import Annotated
-
-class App(BaseSettings):
-    open_ai: Annotated[OpenAi, "OpenAI settings"]
-    weaviate: Annotated[Weaviate, "Weaviate settings"]
--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -1,3 +1,2 @@
-from .App import App
 from .OpenAi import OpenAi
 from .Weaviate import Weaviate
